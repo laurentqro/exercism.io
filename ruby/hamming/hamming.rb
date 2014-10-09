d47a1d5e3151e8685a86a6cd2a4e7533820de896
Laurent Curau <laurentcurau@gmail.com>
2014-10-09 12:24:45 +0100

    Refactor using each_with_index

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,18 +1,12 @@
 class Hamming
-  def self.compute(strand_1, strand_2)
-    strand_1 = strand_1.chars
-    strand_2 = strand_2.chars
+  def self.compute(strand, other_strand)
+    strand = strand.to_s.chars
+    other_strand = other_strand.to_s.chars
 
-    min = (strand_1.length < strand_2.length) ? strand_1.length : strand_2.length
+    max_iterations = [strand.size, other_strand.size].min
 
-    strand_1 = strand_1.take(min)
-    strand_2 = strand_2.take(min)
-
-    hamm = 0
-
-    strand_1.each_index do |index|
-      hamm += 1 if strand_1[index] != strand_2[index]
+    (0...max_iterations).each_with_index.count do |item, index|
+      strand[index] != other_strand[index]
     end
-    hamm
   end
 end
