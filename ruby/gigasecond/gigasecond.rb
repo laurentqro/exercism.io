6446111bc62c51754f7806f80523a38115fd6181
Laurent Curau <laurentcurau@gmail.com>
2014-10-09 12:30:46 +0100

    Make 1 billion seconds notation more readable with underscores

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -5,7 +5,7 @@ class Gigasecond
 
   def self.from(date)
     t = Time.mktime(date.year, date.month, date.day)
-    gs = t + 1000000000
+    gs = t + 1000_000_000
     Date.new(gs.year, gs.month, gs.day)
   end
 
