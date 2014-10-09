32c0a5c27b2a46d2f72288ae14f68f9db1aaeb0b
Laurent Curau <laurentcurau@gmail.com>
2014-10-09 11:57:33 +0100

    Refactor Raindrops to use map method

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,22 +1,10 @@
 module Raindrops
+
   def self.convert(int)
-    case
-      when int%3 == 0 && int%5 == 0 && int%7 == 0
-        "PlingPlangPlong"
-      when int%3 == 0 && int%5 == 0
-        "PlingPlang"
-      when int%3 == 0 && int%7 == 0
-        "PlingPlong"
-      when int%5 == 0 && int%7 == 0
-        "PlangPlong"
-      when int%3 == 0
-        "Pling"
-      when int%5 == 0
-        "Plang"
-      when int%7 == 0
-        "Plong"
-      else
-        int.to_s
+    sounds = {3 => "Pling", 5 => "Plang", 7 => "Plong"}.map do |factor, sound|
+      sound if int%factor == 0
     end
+    sounds.any? ? sounds.join : int.to_s
   end
+
 end
