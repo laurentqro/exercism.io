4cacb052f703a12749487203abcd5dc261862966
Laurent Curau <laurentcurau@gmail.com>
2014-11-26 00:35:23 +0000

    Refactor using #zip. Awesomeness!

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,12 +1,5 @@
 class Hamming
-  def self.compute(strand, other_strand)
-    strand = strand.to_s.chars
-    other_strand = other_strand.to_s.chars
-
-    max_iterations = [strand.size, other_strand.size].min
-
-    (0...max_iterations).each_with_index.count do |item, index|
-      strand[index] != other_strand[index]
-    end
+  def self.compute(dna_strand1, dna_strand2)
+		dna_strand1.chars.zip(dna_strand2.chars).count { |n1, n2| n1 && n2 && n1 != n2 }
   end
 end
